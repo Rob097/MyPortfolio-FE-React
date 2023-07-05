dbb04b70fefc930cea497c4e5a4395fb6920a38e
Roberto dellantonio <roberto.dellantonio@fleap.io>
2023-07-05 15:30:17 +0200

    Started implementing tinymce rich editor text

--- apps/dashboard/src/pages/UserProfile/index.js
+++ apps/dashboard/src/pages/UserProfile/index.js
@@ -1,10 +1,35 @@
 import Header from "./header";
+import React, { useRef } from 'react';
+import TinyEditorComponent from "../../components/TinyEditorComponent";
 
 const UserProfile = () => {
+    const editorRef = useRef(null);
+    const log = () => {
+        if (editorRef.current) {
+            console.log(editorRef.current.getContent());
+        }
+    };
 
     return (
         <>
             <Header />
+
+            <TinyEditorComponent
+                init={{
+                    height: 500,
+                    menubar: true,
+                    plugins: [
+                        'advlist autolink lists link image charmap print preview anchor',
+                        'searchreplace visualblocks code fullscreen',
+                        'insertdatetime media table paste code help wordcount'
+                    ],
+                    toolbar: 'undo redo | formatselect | ' +
+                        'bold italic backcolor | alignleft aligncenter ' +
+                        'alignright alignjustify | bullist numlist outdent indent | ' +
+                        'removeformat | help'
+                }}
+            />
+            <button onClick={log}>Log editor content</button>
         </>
     )
 }
