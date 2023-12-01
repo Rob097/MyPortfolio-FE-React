c2936582073fbcf9c9f968505b196d5c383e6a4f
Roberto dellantonio <roberto.dellantonio@fleap.io>
2023-12-01 16:52:14 +0100

    Removed pricing component for builder

--- apps/application/builder-registry.js
+++ apps/application/builder-registry.js
@@ -64,11 +64,3 @@ Builder.registerComponent(
     },
   }
 );
-
-// register the component "PricingComponent" in /components/switcher/index.js
-Builder.registerComponent(
-  dynamic(() => import("./components/switcher/index")),
-  {
-    name: "PricingComponent",
-  }
-);
