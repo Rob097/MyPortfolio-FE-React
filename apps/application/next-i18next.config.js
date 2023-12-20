e64d8791c1f78d8865fea5784347edf4a8ee1b90
Roberto <roberto.dellantonio@fleap.io>
2023-12-20 08:13:19 +0100

    Disabled locale detection

--- apps/application/next-i18next.config.js
+++ apps/application/next-i18next.config.js
@@ -4,7 +4,8 @@ module.exports = {
     locales: ['en', 'it'],
     // the default locale to be used when visiting
     // a non-localized route (e.g. `/about`)   
-    defaultLocale: 'en'
+    defaultLocale: 'en',
+    localeDetection: false // disable automatic locale detection in the browser. It its true, it will use the browser language as default language
   },
   fallbackLng: {
     default: ['en'],
