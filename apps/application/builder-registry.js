2fa0d59a1e6b0c7a0c2d930ac242e1029bd48064
Roberto <roberto.dellantonio@fleap.io>
2024-01-09 16:50:03 +0100

    Created Accordion component for builder-io

--- apps/application/builder-registry.js
+++ apps/application/builder-registry.js
@@ -57,3 +57,113 @@ Builder.registerComponent(
     },
   }
 );
+
+// Register accordion component (components/builder-io/Accordion.jsx) (Ten elements)
+Builder.registerComponent(
+  dynamic(() => import("./components/builder-io/Accordion")),
+  {
+    name: "Accordion",
+    inputs: [
+      {
+        name: "firstTitle",
+        type: "text",
+        defaultValue: "First Accordion Title",
+      },
+      {
+        name: "firstContent",
+        type: "text",
+        defaultValue: "First Accordion Content",
+      },
+      {
+        name: "secondTitle",
+        type: "text",
+        defaultValue: "Second Accordion Title",
+      },
+      {
+        name: "secondContent",
+        type: "text",
+        defaultValue: "Second Accordion Content",
+      },
+      {
+        name: "thirdTitle",
+        type: "text",
+        defaultValue: "Third Accordion Title",
+      },
+      {
+        name: "thirdContent",
+        type: "text",
+        defaultValue: "Third Accordion Content",
+      },
+      {
+        name: "fourthTitle",
+        type: "text",
+        defaultValue: "Fourth Accordion Title",
+      },
+      {
+        name: "fourthContent",
+        type: "text",
+        defaultValue: "Fourth Accordion Content",
+      },
+      {
+        name: "fifthTitle",
+        type: "text",
+        defaultValue: "Fifth Accordion Title",
+      },
+      {
+        name: "fifthContent",
+        type: "text",
+        defaultValue: "Fifth Accordion Content",
+      },
+      {
+        name: "sixthTitle",
+        type: "text",
+        defaultValue: "Sixth Accordion Title",
+      },
+      {
+        name: "sixthContent",
+        type: "text",
+        defaultValue: "Sixth Accordion Content",
+      },
+      {
+        name: "seventhTitle",
+        type: "text",
+        defaultValue: "Seventh Accordion Title",
+      },
+      {
+        name: "seventhContent",
+        type: "text",
+        defaultValue: "Seventh Accordion Content",
+      },
+      {
+        name: "eighthTitle",
+        type: "text",
+        defaultValue: "Eighth Accordion Title",
+      },
+      {
+        name: "eighthContent",
+        type: "text",
+        defaultValue: "Eighth Accordion Content",
+      },
+      {
+        name: "ninthTitle",
+        type: "text",
+        defaultValue: "Ninth Accordion Title",
+      },
+      {
+        name: "ninthContent",
+        type: "text",
+        defaultValue: "Ninth Accordion Content",
+      },
+      {
+        name: "tenthTitle",
+        type: "text",
+        defaultValue: "Tenth Accordion Title",
+      },
+      {
+        name: "tenthContent",
+        type: "text",
+        defaultValue: "Tenth Accordion Content",
+      }
+    ],
+  }
+);
